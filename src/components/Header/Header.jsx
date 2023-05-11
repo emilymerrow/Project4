eb072bfdcf1d1e7e5fd53ed8d3d29442f93f33b8
emilymerrow <emilyanddavidmerrow@gmail.com>
2023-05-11 13:35:55 -0400

    more css transfer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,45 +3,35 @@ import { Header, Segment, Image, Icon } from "semantic-ui-react";
 import "./Header.css"; // Import the Header.css file if you haven't already
 
 function PageHeader({ loggedUser, handleLogout }) {
-    return (
-      <Segment clearing className="main-header">
-        {/* User info */}
-        <div className="user-info">
-          <Link to={`/${loggedUser?.username}`}>
-            <Image
-              src={
-                loggedUser?.photoUrl
-                  ? loggedUser?.photoUrl
-                  : "https://react.semantic-ui.com/images/wireframe/square-image.png"
-              }
-              avatar
-              className="avatar"
-            ></Image>
-          </Link>
-          {/* Display the username */}
-          <Header as="h3" floated="left" className="username">
-            {loggedUser?.username}
-          </Header>
-        </div>
-  
-        {/* Logo */}
-        <Header as="h2" className="logo">
-          Chores Buster
-        </Header>
-  
-        {/* Navigation links */}
-        <Header as="h2" floated="right" className="nav-links">
-          <Link to="/">
-            <Icon name="home"></Icon>
-          </Link>
-          <Link to="" onClick={handleLogout}>
-            Logout
-          </Link>
-        </Header>
-      </Segment>
-    );
-  }
-  
-  
-  
+  return (
+    <Segment clearing className="page-header">
+      <Header as="h2" floated="right">
+        <Link to="/">
+          <Icon name="home"></Icon>
+        </Link>
+        <Link to="" onClick={handleLogout}>
+          Logout
+        </Link>
+      </Header>
+      <Header as="h2" floated="left">
+        <Link to={`/${loggedUser?.username}`}>
+          <Image
+            src={
+              loggedUser?.photoUrl
+                ? loggedUser?.photoUrl
+                : "https://react.semantic-ui.com/images/wireframe/square-image.png"
+            }
+            avatar
+            className="header-avatar"
+          ></Image>
+        </Link>
+      </Header>
+      {/* Add the Chores and Allowance App logo */}
+      <Header as="h2" floated="" className="logo">
+        Chores Buster App
+      </Header>
+    </Segment>
+  );
+}
+
 export default PageHeader;
